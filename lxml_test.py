733b1a91b148c93535bd118f3c9092a5db6eda24
Kota Tsuyuzaki <tsuyuzaki.kota@lab.ntt.co.jp>
2014-11-19 01:06:33 -0800

    commit

--- lxml_test.py
+++ lxml_test.py
@@ -6,24 +6,23 @@ class _Element(etree.ElementBase):
         super(_Element, self).__init__(*args, **kwargs)
 
     @property
-    def utf8_text(self):
+    def text(self):
         '''
         utf-8 wrapper property of lxml.etree.Element.text
-
-        Note that this property has a same value with lxml.etree.Element.text
-        internally but it returns utf-8 encoded string instead of unicode when
-        getter called. It also allows both str and unicode format for an input.
         '''
-        text = self.text
+
+        text = etree.ElementBase.text.__get__(self)
         if isinstance(text, unicode):
             text = text.encode('utf-8')
         return text
 
-    @utf8_text.setter
-    def utf8_text(self, value):
+    @text.setter
+    def text(self, value):
         if isinstance(value, str):
             value = value.decode('utf-8')
-        self.text = value
+        #setattr(etree.ElementBase, 'text', value)
+        #etree._Element.text.__set__(self, value)
+        etree.ElementBase.text.__set__(self, value)
 
 
 parser_lookup = etree.ElementDefaultClassLookup(element=_Element)
@@ -37,10 +36,11 @@ if __name__ == '__main__':
     elem = Element('Test')
     sub = SubElement(elem, 'FOO')
   
-    sub.utf8_text = '\xef\xbc\xa1'
+    sub.text = '\xef\xbc\xa1'
     print sub.text
+    print type(sub.text)
     xml_string = etree.tostring(elem, xml_declaration=True, encoding='UTF-8')
     print xml_string
     elem = etree.fromstring(xml_string, parser)
-    print elem.find('FOO').utf8_text
+    print elem.find('FOO').text
     print isinstance(elem, _Element)
